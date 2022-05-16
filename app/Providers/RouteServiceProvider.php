10777f696a0fd8230c086b3b025798b96557f828
cmunhozprog03 <cmunhozprog03@hotmail.com>
2022-05-16 09:05:42 -0300

    Create Model Plan

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -33,9 +33,13 @@ class RouteServiceProvider extends ServiceProvider
                 ->middleware('api')
                 ->group(base_path('routes/api.php'));
 
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
-        });
+                Route::middleware('web')
+                    ->group(base_path('routes/web.php'));
+
+                Route::middleware('web')
+                    ->prefix('admin')
+                    ->group(base_path('routes/admin.php'));
+            });
     }
 
     /**
